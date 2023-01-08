5edc864f7d736effbef0ffa970ab0884b8751633
Karina Garces ✨ <karinadgarces@gmail.com>
2023-01-07 22:27:28 -0500

    adding item test passes

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,15 +2,18 @@ from typing import List
 
 class ShoppingCart:
     def __init__(self) -> None:
+        self.items: List[str] = []
         pass
     
     def add(self, item: str):
+        self.items.append(item)
         pass
     
     def size(self) -> int:
-        return 0
+        return len(self.items)
     
     def get_items(self) -> List[str]:
+        return self.items
         pass
     
     def get_total_price(self, price_map):
