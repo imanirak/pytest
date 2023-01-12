ef2f1201541b587c4e44a2954133932b8c9b1bdc
Karina Garces ✨ <karinadgarces@gmail.com>
2023-01-12 10:23:40 -0500

    clean up

--- test_saml_response/saml_data.py
+++ test_saml_response/saml_data.py
@@ -1,7 +1,4 @@
-import json
 import xmltodict
-from typing import List
-import pprint
 
 class SamlResponse:
     #convert xml to dict
@@ -12,24 +9,23 @@ class SamlResponse:
     # verify certificate is present in response     
     def verify_certificate(self):
         doc = self.doc
-        for i in doc.items():
-            cert = doc["saml2p:Response"]["ds:Signature"]["ds:KeyInfo"]["ds:X509Data"]["ds:X509Certificate"]
+        cert = doc["saml2p:Response"]["ds:Signature"]["ds:KeyInfo"]["ds:X509Data"]["ds:X509Certificate"]
         return cert
     
     # verify signature is present in response
     def verify_signature(self):
         doc = self.doc
-        for i in doc.items():
-            sig = doc["saml2p:Response"]["ds:Signature"]["ds:SignatureValue"]
+        sig = doc["saml2p:Response"]["ds:Signature"]["ds:SignatureValue"]
         return sig
     
     # verify success status    
     def saml2p_status_code(self):
         doc = self.doc
-        for i in doc.items():
-            status = doc["saml2p:Response"]["saml2p:Status"]["saml2p:StatusCode"]["@Value"]
-            
+        status = doc["saml2p:Response"]["saml2p:Status"]["saml2p:StatusCode"]["@Value"]  
         return status
 
 test = SamlResponse("saml_test_data.xml")
-
+print(test.doc)
+# print(test.verify_certificate())
+# print(test.saml2p_status_code())
+# print(test.verify_signature())
